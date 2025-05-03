4402ef61d376ccd60b333f218f55dc76a3ab7cf5
andrew <andrewayiko40@gmail.com>
2025-05-03 12:35:06 +0300

    feat: add Test_CreateAccount function for account creation testing

---
+++ api/create_test.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Ayikoandrew/server/database"
+	"github.com/Ayikoandrew/server/types"
+)
+
+func Test_CreateAccount(t *testing.T) {
+	os.Setenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/liora?sslmode=disable")
+	// url := os.Getenv("TEST_DATABASE_URL")
+	store := database.NewStorage()
+
+	if err := store.Init(); err != nil {
+		t.Fatalf("Failed to initialize database %s", err)
+	}
+
+	defer store.Close()
+
+	testAccount := &types.Account{
+		FirstName:   "Test",
+		LastName:    "User",
+		PhoneNumber: "1234567890",
+		Email:       "test@example.com",
+		Password:    []byte("password123"),
+	}
+
+	if err := store.CreateAccount(testAccount); err != nil {
+		t.Fatalf("Failed to create account: %v", err)
+	}
+}
