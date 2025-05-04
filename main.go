ae782d03ce4614a2ae7f2fa142236f52ea7746e2
andrew <andrewayiko40@gmail.com>
2025-05-04 15:14:39 +0300

    fix: update server port configuration to use environment variable

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"log"
 	"log/slog"
+	"os"
 
 	"github.com/Ayikoandrew/server/api"
 	"github.com/Ayikoandrew/server/database"
 )
 
 func main() {
-	listenAddr := ":40000"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	store := database.NewStorage()
 	if err := store.Init(); err != nil {
 		slog.Error("error creating database table", "err", err)
 	}
-	server := api.NewServer(listenAddr, store)
+	server := api.NewServer(":"+port, store)
+	log.Printf("Server starting on port %s...", port)
 
 	server.Run()
 }
