e0e553722c43a3fd5714724317e2e582a3882861
andrew <andrewayiko40@gmail.com>
2025-05-07 13:17:33 +0300

    feat: update server port configuration to use environment variable with default fallback

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"log/slog"
+	"os"
 
 	"github.com/Ayikoandrew/server/api"
 	"github.com/Ayikoandrew/server/database"
 )
 
 func main() {
-	listenAddr := ":40000"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	store := database.NewStorage()
 	if err := store.Init(); err != nil {
 		slog.Error("error creating database table", "err", err)
 	}
-	server := api.NewServer(listenAddr, store)
+	server := api.NewServer(":"+port, store)
 
 	server.Run()
 }
