99965176a37983c79884c233e083199441fbb37c
andrew <andrewayiko40@gmail.com>
2025-05-09 19:13:35 +0300

    initialized redis

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
 
@@ -9,10 +10,20 @@ import (
 )
 
 func main() {
+	slog.Info("Starting server...")
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+	database.InitRedis()
+
+	client := database.GetRedisClient()
+	if err := client.Ping(context.Background()).Err(); err != nil {
+		slog.Error("Failed to connect to Redis", "error", err)
+		os.Exit(1)
+	}
+	slog.Info("Successfully connected to Redis")
+
 	store := database.NewStorage()
 	if err := store.Init(); err != nil {
 		slog.Error("error creating database table", "err", err)
