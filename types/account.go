2c1707b4ab4adf864248c29d53e6c61b2f8a0d19
andrew <andrewayiko40@gmail.com>
2025-05-09 19:14:38 +0300

    introduced new models User, LoginRequest and LoginResponse

--- types/account.go
+++ types/account.go
@@ -5,5 +5,25 @@ type Account struct {
 	LastName    string `json:"lastName"`
 	PhoneNumber string `json:"phoneNumber"`
 	Email       string `json:"email"`
-	Password    []byte `json:"passwordHash"`
+	Password    string `json:"password"`
+}
+
+type User struct {
+	ID          string `json:"id"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	PhoneNumber string `json:"phoneNumber"`
+	Email       string `json:"email"`
+	Password    string `json:"-"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	User         User   `json:"user"`
 }
