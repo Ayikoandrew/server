1e56df677f760c3dbd19f5b85687c66bdaddf4fb
andrew <andrewayiko40@gmail.com>
2025-05-11 08:54:29 +0300

    Refactor main function to streamline Redis initialization and connection handling

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"context"
 	"log/slog"
 	"os"
-	"time"
 
 	"github.com/Ayikoandrew/server/api"
 	"github.com/Ayikoandrew/server/database"
@@ -12,26 +10,20 @@ import (
 
 func main() {
 	slog.Info("Starting server...")
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	database.InitRedis()
 
-	client := database.GetRedisClient()
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := client.Ping(ctx).Err(); err != nil {
-		slog.Error("Failed to connect to Redis", "error", err, "url", os.Getenv("REDIS_URL"))
-		os.Exit(1)
-	}
-	slog.Info("Successfully connected to Redis")
+	database.InitRedis()
 
 	store := database.NewStorage()
 	if err := store.Init(); err != nil {
 		slog.Error("error creating database table", "err", err)
+		os.Exit(1)
 	}
-	server := api.NewServer(":"+port, store)
 
+	server := api.NewServer(":"+port, store)
 	server.Run()
 }
