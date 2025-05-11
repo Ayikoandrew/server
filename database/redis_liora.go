b1fdaa5edd2f5801b0bfcbbae8555dfd57430636
andrew <andrewayiko40@gmail.com>
2025-05-11 08:16:33 +0300

    Refactor NewRDB function to enhance Redis client configuration with retry options

--- database/redis_liora.go
+++ database/redis_liora.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"fmt"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -14,11 +16,25 @@ var (
 
 func NewRDB() *redis.Client {
 	url := os.Getenv("REDIS_URL")
+	if url == "" {
+		redisPassword := os.Getenv("REDIS_PASSWORD")
+		redisPort := os.Getenv("REDIS_PORT")
+
+		if redisPassword != "" {
+			url = fmt.Sprintf("redis://:%s@redis:%s/0", redisPassword, redisPort)
+		} else {
+			url = fmt.Sprintf("redis://redis:%s/0", redisPort)
+		}
+	}
 	opts, err := redis.ParseURL(url)
 	if err != nil {
 		panic(err)
 	}
-	return redis.NewClient(opts)
+	client := redis.NewClient(opts)
+	client.Options().MaxRetries = 5
+	client.Options().MinRetryBackoff = 100 * time.Millisecond
+	client.Options().MaxRetryBackoff = 2 * time.Millisecond
+	return client
 }
 
 func GetRedisClient() *redis.Client {
